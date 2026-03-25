119b216b325de075b2b6b22a2a4497f9f84f82ad
joegoico <goicoecheajosem@gmail.com>
2026-03-24 21:24:36 -0300

    procesando tecnicas de preprocesamiento

--- backend/custom/custom_classes.py
+++ backend/custom/custom_classes.py
@@ -1,17 +1,22 @@
 from langchain_core.prompts import PromptTemplate
 from pydantic import BaseModel, Field
-from engine import model as llm
+from backend.engine import model as llm
 
 class TableSelector(BaseModel):
     table_name: list[str] = Field(..., description="The name of the table to query")
 
 # Instanciamos a Groq pero le ponemos el "bozal" que creamos arriba
-llm_estricto = llm.with_structured_output(TableSelector)
+llm_estricto = llm.with_structured_output(TableSelector, method="json_mode")
 
 # El prompt del recepcionista
 prompt_recepcionista = PromptTemplate.from_template("""
 Sos un experto en bases de datos. Dada la pregunta del usuario, devolvé SOLO la lista de tablas que hacen falta.
 Tus únicas opciones son: autor, cliente, condicion, editorial, libro, titulo, venta, volumen.
+INSTRUCCIÓN DE FORMATO:
+Debes responder ÚNICAMENTE con un objeto JSON que siga esta estructura:
+{{
+    "table_name": ["nombre_tabla1", "nombre_tabla2"]
+}}
 
 Pregunta del usuario: {pregunta}
 """)
